4340f3be3b83700dff2a67b611ed40d812b25fdb
Jake <jgalvin6@jhu.edu>
2023-09-07 19:45:54 -0400

    Add part 4

--- day3-homework/mean_inflammation.py
+++ day3-homework/mean_inflammation.py
@@ -2,8 +2,9 @@
 
 import sys
 
-p = sys.argv[1]
-f = open(sys.argv[2])
+p1 = sys.argv[1]
+p2 = sys.argv[2]
+f = open(sys.argv[3])
 lines = f.readlines()
 
 def meanInflammation(patient, file):
@@ -13,6 +14,19 @@ def meanInflammation(patient, file):
 	for i in range(len(patientData)):
 		iSum += float(patientData[i])
 	iAvg = iSum/len(patientData)
-	return iAvg
+	print("Patient", patient, "average inflammation: ", iAvg)
 
-print(meanInflammation(p,f))
+def differenceInflammation(patient1, patient2, file):
+	patient1 = int(patient1)
+	patient2 = int(patient2)
+
+	patient1Data = lines[patient1].split(",")
+	patient2Data = lines[patient2].split(",")
+
+	for i in range(len(patient1Data)):
+		diff = abs(int(patient1Data[i]) - int(patient2Data[i]))
+		print("Day", i + 1, ": ", diff)
+
+
+meanInflammation(p1,f)
+differenceInflammation(p1, p2, f)
